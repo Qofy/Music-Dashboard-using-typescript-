30af8fd5b5ef14f4fe3c8932846d546f9575bdd4
Qofy <148817944+Qofy@users.noreply.github.com>
2025-03-27 11:08:50 +0100

    adding react rout oulet to navigate on a single page, but there is a proble on the home button.(fix it later)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Browse } from "./pages/Browse"
-import { Favorite } from "./pages/Favorite"
-import { Library } from "./pages/Library"
-import { NotFound } from "./pages/NotFound"
-
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./component/Layout";
+import { MiddleContext } from "./context/Middlecontext";
+import { Browse } from "./pages/Browse";
+import { Favorite } from "./pages/Favorite";
+import { Library } from "./pages/Library";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
-
   return (
     <div className="main">
-    {/* <Home/> */}
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="browse" element={<Browse/>}/>
-      <Route path="favorite" element={<Favorite/>}/>
-      <Route path="library" element={<Library/>}/>
-      <Route path="*" element={<NotFound/>}/>
-
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MiddleContext />} />
+          <Route path="browse" element={<Browse />} />
+          <Route path="favorite" element={<Favorite />} />
+          <Route path="library" element={<Library />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
