62ada6ac3faaaecf6d507a4d3b81dbbd8700177d
Qofy <148817944+Qofy@users.noreply.github.com>
2025-04-25 11:28:26 +0200

    starting library page

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,16 +1,29 @@
 import "../scss/pages/library.scss"
 
+const libraryHeadStyl= {
+  display: "flex",
+  justifyContent:"space-between",
+  marginTop:"2rem",
+  alignItems: "center"
+}
+const subConStyl = {
+  marginTop:"4.4rem",
+  marginBottom: "6rem",
+}
+
 export function Library (){
   return(
     <div className="library-container">
-      <h2>Library</h2>
-      <div className="library__head">
-        <div className="library__btns">
-        <button></button>
-        <button></button>
-        <button></button>
+      <div className="library-sub__Container" style={subConStyl}>
+      <h2 className="library-name">Library</h2>
+      <div className="library__head" style={libraryHeadStyl}>
+        <div className="library__p">
+        <p>Artist</p>
+        <p>Album</p>
+        <p>Songs</p>
         </div>
         <p className="sort">Sort by Name</p>
+      </div>
       </div>
     </div>
   )
